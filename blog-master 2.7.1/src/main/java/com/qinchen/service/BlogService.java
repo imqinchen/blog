9c2decbf0796089f957f74e4128b9fd1999d965b
imqinchen <69503948+imqinchen@users.noreply.github.com>
2021-12-14 20:03:46 +0800

    Update BlogService.java

--- blog-master 2.7.1/src/main/java/com/qinchen/service/BlogService.java
+++ blog-master 2.7.1/src/main/java/com/qinchen/service/BlogService.java
@@ -14,17 +14,7 @@ public interface BlogService {
     public List<Blog> searchBlog(Blog blog);
 
 
-    public Integer countBlog();
-    public Map<String,List<Blog>> archiveBlog();
 
-
-    public List<Blog> getBlogByTypeId(Long typeId);
-
-
-    public List<Blog> getIndexBlog();
-    public List<Blog> getRecommendBlog();
-    public List<Blog> searchIndexBlog(String query);
-    public Blog getDetailedBlog(Long id);
 
 
 }
